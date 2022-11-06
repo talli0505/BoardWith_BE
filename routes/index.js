baae977a158850f982ccc3ac4141cb6ce0c6cc76
Seosoohyung <spartaseosu@gmail.com>
2022-11-06 15:14:14 +0900

    2022-11-06 PM03:14 Posts 오류 수정 서수형

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const usersRouter = require("./users.router");
 const postsRouter = require("./posts.router");
 const commentsRouter = require("./comments.router");
+// const chatRouter = require("./chat.router");
 
+// router.use("/chat", chatRouter)
 router.use("/users", usersRouter);
 router.use("/posts", postsRouter)
 router.use("/comments", commentsRouter)
