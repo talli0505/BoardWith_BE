1afa69071b9d78adede2ec3df95382ad3a7d00df
ihyun <okyee8335@naver.com>
2022-11-19 22:45:41 +0900

    2022-11-19 PM22:45 Feat)Posts Key Word Search(by.Title&Nickname) 백이현

--- services/posts.js
+++ services/posts.js
@@ -4,7 +4,7 @@ const Posts = require("../schema/posts")
 class PostsService {
     postsRepository = new PostsRepository();
 
-    createPosts = async( userId, img, nickName, title, content, location, cafe, date, time, map, partyMember, participant, nowToClose) => {     
+    createPosts = async( userId, img, nickName, title, content, location, cafe, date, time, map, partyMember, participant, nowToClose) => {
         if(!title || !content || !location || !cafe || !date || !time || !map || !partyMember){
             const err = new Error('postService Error');
             err.status = 403;
@@ -13,9 +13,10 @@ class PostsService {
         } else {
             await this.postsRepository.createPosts(userId, img, nickName, title, content, location, cafe, date, time, map, partyMember, participant, nowToClose)
         }
-        return    
+        return
     }
 
+    //게시글 키워드 검색
     searchPost = async(keyword) => {
         const searchPost = await this.postsRepository.searchPost(keyword)
         return searchPost
@@ -27,23 +28,23 @@ class PostsService {
     }
 
     findOnePost = async(postId) => {
-        try{   
-        const findOnePosts = await this.postsRepository.findOnePost(postId);
-        return findOnePosts;
-        }catch(err){        
+        try{
+            const findOnePosts = await this.postsRepository.findOnePost(postId);
+            return findOnePosts;
+        }catch(err){
             err.status = 404
             err.message = "게시물이 없습니다."
             throw err;
-        }               
+        }
     }
 
-    updatePost = async(postId, userId, title, content, location, cafe, date, time, map, partyMember) => { 
+    updatePost = async(postId, userId, title, content, location, cafe, date, time, map, partyMember) => {
         if(!title || !content || !location || !cafe || !date || !time || !map || !partyMember){
             const err = new Error('postService Error');
             err.status = 403;
             err.message = "빈칸을 입력해주세요."
             throw err;
-        }  
+        }
         const findOnePost = await this.postsRepository.findOnePost(postId)
         if(findOnePost._id.toString() !== postId){
             const err = new Error('postService Error');
@@ -52,27 +53,27 @@ class PostsService {
             throw err
         }
         await this.postsRepository.updatePost(postId, userId, title, content, location, cafe, date, time, map, partyMember)
-        return 
+        return
     }
-    
+
     deletePost = async(postId, userId) => {
-        try{        
-        await this.postsRepository.deletePost(postId, userId);
-        return 
+        try{
+            await this.postsRepository.deletePost(postId, userId);
+            return
         }catch(err){
             err.status = 404
             err.message = "경로 요청이 잘못되었습니다."
             throw err
-        }      
+        }
     }
 
-    participateMember = async (postId,userId, nickName) => {                 
+    participateMember = async (postId,userId, nickName) => {
         const findMembersLength = await this.postsRepository.findOnePost(postId)
         if( findMembersLength.partyMember <= (findMembersLength.participant.length - 1) ){
-            throw "참가 마감되었습니다."            
+            throw "참가 마감되었습니다."
         } else if( findMembersLength.partyMember > findMembersLength.participant.length ){
-            await this.postsRepository.participateMember(postId, userId, nickName) 
-        }  
+            await this.postsRepository.participateMember(postId, userId, nickName)
+        }
         return
     }
 
@@ -80,17 +81,17 @@ class PostsService {
     confirmMember = async (postId, nickName) => {
         const confirmMember = await this.postsRepository.confirmMember(postId, nickName)
         if(confirmMember.participant.includes(nickName)) {
-           await this.postsRepository.pushconfirmMember(postId, nickName)                       
+            await this.postsRepository.pushconfirmMember(postId, nickName)
         } else {
             await this.postsRepository.pullconfirmMember(postId,nickName)
-        }        
-        return confirmMember       
+        }
+        return confirmMember
     }
 
     banMember = async(postId,nickName) => {
         await this.postsRepository.banMember(postId, nickName)
         return
-    } 
+    }
 
     cancelBanMember = async(postId, nickName) => {
         await this.postsRepository.cancelBanMember(postId, nickName)
@@ -149,7 +150,7 @@ class PostsService {
             await this.postsRepository.pushBookmark(postId, nickName)
         } else if(findBookmark.postId.includes(postId)){
             await this.postsRepository.pullBookmark(postId, nickName)
-        }        
+        }
         return findBookmark
     }
 
@@ -158,22 +159,19 @@ class PostsService {
         const getBookmark = await this.postsRepository.getBookmark(nickName);
         const GetBookmark = getBookmark.map((p) => p.postId)
         for (let i = 0; i < GetBookmark.length; i++){
-           const AllgetBookmark = await this.postsRepository.AllgetBookmark(GetBookmark[i])
-           if(AllgetBookmark.length === 0) {
-            const err = new Error('postsService Error');
-            err.status = 200
-            err.message = "등록된 게시물이 없습니다."
-            throw err
+            const AllgetBookmark = await this.postsRepository.AllgetBookmark(GetBookmark[i])
+            if(AllgetBookmark.length === 0) {
+                const err = new Error('postsService Error');
+                err.status = 200
+                err.message = "등록된 게시물이 없습니다."
+                throw err
             } else if(AllgetBookmark.length !== 0){
                 result.push(AllgetBookmark)
             }
             console.log(AllgetBookmark.length)
-        }        
-        return result        
+        }
+        return result
     }
-
-
-
 }
 
 module.exports = PostsService;
